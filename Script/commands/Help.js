e39c1cfec7083c387b5d8f4209a909a44568a90d
fbboot <fbboot587@gmail.com>
2026-06-05 00:08:27 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -44,7 +44,7 @@ module.exports.languages = {
 // 🔹 এখানে আপনার ফটো Imgur লিংক করে বসাবেন ✅
 const helpImages = [
     
-    "https://imgur.com/a/ovM0Q7H"
+    "https://imgur.com/a/ovM0Q7H.jpg"
 ];
 
 
